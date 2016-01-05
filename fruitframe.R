11d419ea82d1948673cd65050357213d3b13a516
Chris Powell <sooty@umich.edu>
2016-01-05 15:13:09 -0500

    sharing the fruit

---
+++ fruitframe.R
@@ -0,0 +1,3 @@
+#create a new data frame with three rows and three columns
+
+nf<-data.frame("first name"=c('Dan', 'Fiona', 'Duncan'),"last name"=c('Powell', 'Powell','Powell'),"fav fruit"=c('banana', 'pomegranate', 'strawberry'))
