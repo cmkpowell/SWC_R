f79d055e2c6ad0f191f794956914bc51988bdc42
Chris Powell <sooty@umich.edu>
2016-01-05 11:23:56 -0500

    Adding commented header

---
+++ variables.R
@@ -0,0 +1,14 @@
+# This is the document were I learn introductory R.
+
+x <- 1:10 #an array of integers from 1-10
+y <- x*2
+z <- y^2
+x + y + z
+
+mass <- 47.5
+age <- 122
+mass <- mass * 2.3
+age <- age - 20
+
+mass==age
+mass>age
